41f5bb81739a1f38aeadec8959fd03d842b836ac
Nileshlad <60500269+Nileshlad@users.noreply.github.com>
2020-05-29 11:46:47 +0530

    Added: To welcome massege in greeting application

---
+++ src/main/java/com/springgreettingappdevelopment/SpringGreettingAppDevelopmentApplication.java
@@ -0,0 +1,14 @@
+package com.springgreettingappdevelopment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGreettingAppDevelopmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGreettingAppDevelopmentApplication.class, args);
+        System.out.println("Welcome greeting application ");
+    }
+
+}
