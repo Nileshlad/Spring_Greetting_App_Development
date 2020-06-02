fece31656549e31920015de7d17fdb3553137366
Nileshlad <60500269+Nileshlad@users.noreply.github.com>
2020-06-02 16:01:16 +0530

    Modified: Method to get greeting with first or last name based on attributes provided by user

--- src/main/java/com/restservice/GreetingServiceImpl.java
+++ src/main/java/com/restservice/GreetingServiceImpl.java
@@ -8,6 +8,12 @@ public class GreetingServiceImpl implements IGreetingService {
     private static final String template = "Hello world!";
 
     public String greet(String firstName, String lastName) {
+        if (firstName == null && lastName == null)
+            return template;
+        else if (lastName == null)
+            return template.replace("world", firstName);
+        else if (firstName == null)
+            return template.replace("world", lastName);
         return template.replace("world", firstName + " " + lastName);
 
     }
