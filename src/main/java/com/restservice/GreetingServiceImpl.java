54626cd9197e5a6bf46ece7d2a07fa063e3210c7
Nileshlad <60500269+Nileshlad@users.noreply.github.com>
2020-06-02 15:55:10 +0530

    Refactored: To method to get greet with first name and last name

--- src/main/java/com/restservice/GreetingServiceImpl.java
+++ src/main/java/com/restservice/GreetingServiceImpl.java
@@ -7,7 +7,8 @@ public class GreetingServiceImpl implements IGreetingService {
 
     private static final String template = "Hello world!";
 
-    public String greet(String name) {
-        return template;
+    public String greet(String firstName, String lastName) {
+        return template.replace("world", firstName + " " + lastName);
+
     }
 }
